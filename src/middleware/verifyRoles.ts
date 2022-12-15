256e3805f7e65c2b0c06a7c966b12359e9ecf375
RutvikrajTorinit <rutvikrajsinh.champavat@torinit.ca>
2022-12-15 13:37:22 +0530

    Integrated authorization for admin and user

---
+++ src/middleware/verifyRoles.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+interface CustomRequest extends Request {
+  roles?: number[];
+}
+
+export const verifyRoles = (...allowedRoles: number[]) => {
+  return (req: CustomRequest, res: Response, next: NextFunction) => {
+    if (!req?.roles) return res.sendStatus(401);
+
+    const rolesArr = [...allowedRoles];
+
+    const result = req.roles
+      ?.map((role: number) => rolesArr?.includes(role))
+      .find((value: boolean) => value === true);
+
+    if (!result) return res.sendStatus(401);
+    next();
+  };
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,15 +1,21 @@
 import express from "express";
+import { ROLES } from "../config/roles_list";
 import {
   deleteUser,
   getUsers,
   getUser,
   updateUser,
 } from "../controllers/userController";
+import { verifyRoles } from "../middleware/verifyRoles";
 
 const userRouter = express.Router();
 
-userRouter.get("/", getUsers);
+userRouter.get("/", verifyRoles(ROLES.user, ROLES.user), getUsers);
 
-userRouter.route("/:id").put(updateUser).delete(deleteUser).get(getUser);
+userRouter
+  .route("/:id")
+  .get(verifyRoles(ROLES.user, ROLES.user), getUser)
+  .put(verifyRoles(ROLES.user, ROLES.user), updateUser)
+  .delete(verifyRoles(ROLES.admin), deleteUser);
 
 export default userRouter;
